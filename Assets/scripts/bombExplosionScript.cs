0311578e3a4727e7d9bcc7e54af48825e60189e5
romGame <rspenato@gmail.com>
2013-10-20 23:30:22 +0200

    bombe mise à jour

--- Assets/scripts/bombExplosionScript.cs
+++ Assets/scripts/bombExplosionScript.cs
@@ -6,6 +6,8 @@ public class bombExplosionScript : MonoBehaviour {
 	// Attributs
 	float timeBeforeExplode = 3f;
 	float timeInWait = 0f;
+	bool bombActivated = false;
+	bool isTicking = false;
 	
 	Animation bombTickingAnimation = null;
 	public Animation BombTicking
@@ -28,35 +30,75 @@ public class bombExplosionScript : MonoBehaviour {
         set { bombExplosionParticle = value; }
     }
 	
+	Transform bombTransform = null;
+	public Transform BombTransform
+    {
+        get { return bombTransform; }
+        set { bombTransform = value; }
+    }
+	
 	// Use this for initialization
 	void Start () {
 		// Retrieve the bomb ticking animation and launch it
 		bombTickingAnimation = this.GetComponent<Animation>();
-		BombTicking.Play();
+		
+		// Retrieve the bom transform property
+		bombTransform = this.GetComponent<Transform>();
 		
 		// Retrieve the explosion sound and store it
 		bombExplosionSound = this.GetComponent<AudioSource>();
 		
 		// Retrieve the Particle System and store it
 		bombExplosionParticle = this.GetComponent<ParticleSystem>();
+		
+		// Activate the bomb (for test because after, it will be the user which will activate a bomb)
+		bombActivated = true;
 	}
 	
 	
 	// Update is called once per frame
 	void Update () {
-		// time in Wait incrementation :
-		timeInWait += Time.deltaTime;
+		if(bombActivated) {
+			
+			if(isTicking == false) {
+				isTicking = true;
+				BombTicking.Play("bombTickingAnimation");
+			}
+			
+			// time in Wait incrementation :
+			timeInWait += Time.deltaTime;
 		
-		// If the time is up, we launch the explosion sound and animation
-		if(timeInWait >= timeBeforeExplode) {
-			// Play the Explosion Sound.
-			BombExplosionSound.Play();
-			
-			// PLay the Explosion Particle
-			BombExplosionParticle.Play();
-			
-			// Reset the timer for that bomb
-			timeInWait = 0f;
+			// If the time is up, we launch the explosion sound and animation
+			if(timeInWait >= timeBeforeExplode) {
+				// Desactivate the ticking and the animation
+				isTicking = false;
+				BombTicking.Stop("bombTickingAnimation");
+				
+				// Launch the explosion animation :
+				StartCoroutine(ExplosionAnim());
+				
+				// Reset the timer for that bomb
+				timeInWait = 0f;
+				
+				// Desactivate the bomb
+				bombActivated = false;
+			}
 		}
+		
 	}
+	
+	IEnumerator ExplosionAnim() {
+        // Play the Explosion Sound.
+		BombExplosionSound.Play();
+			
+		// PLay the Explosion Particle
+		BombExplosionParticle.Play();
+ 
+        yield return new WaitForSeconds(2f);
+				
+		// Move it in another place to make it disappear
+		bombTransform.Translate(new Vector3(100f, 100f, 100f));
+ 
+        Debug.Log("Bomb Explosion Finished");
+    }
 }
